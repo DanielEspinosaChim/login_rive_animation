70a73d91139e6a7b514fb2b04bf81d126dca01c6
Danye <danye@example.com>
2026-09-08 13:42:52 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,15 +8,67 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
+
 class _LoginScreenState extends State<LoginScreen> {
+  //control para ocultal y mostrar contraseña
+  //el giuon bajo es ina varaible ams pricada
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
-    //scaffold: es un widget que proporciona una estructura básica para la aplicación, como una barra de navegación, un cajón de navegación y un cuerpo, como el esqueleto de la aplicación
-    //andamio
+
+    //para obtener el tamaño de la pantalla
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [Expanded(child: RiveAnimation.asset('assets/login_bear.riv'))],
-      )
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                width: size.width,
+                child: RiveAnimation.asset('assets/login_bear.riv'),
+              ), // SizedBox
+              //para separa widgets
+              SizedBox(height: 10),
+              //campo de texto para el email
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration( 
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              //campo de texto para la contraseña
+              TextField(
+                obscureText:  _obscureText, //para ocultar la contraseña
+                decoration: InputDecoration( 
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      //el if ternario es para escribir un if else de fomra sencilla o corta
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ), // Column
+        ), // Padding
+      ),
     );
   }
 }
